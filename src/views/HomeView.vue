81a7587c72d6e2a08f94bbfb39efbf156cf4b062
Anthony <24651774+anthonyaquino83@users.noreply.github.com>
2024-03-20 16:53:59 -0300

    initial

---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { useRouter } from 'vue-router'
+import { useToast } from 'vue-toastification'
+
+const router = useRouter()
+const toast = useToast()
+
+function goToAbout() {
+  router.push('/about')
+}
+function showToast(isSuccess = true) {
+  if (isSuccess) {
+    toast.info("I'm an info toast!")
+  } else {
+    toast.error("I'm an error toast!")
+  }
+}
+</script>
+
+<template>
+  <h2>HomeView</h2>
+  <div class="div" style="margin-bottom: 8px">
+    <button @click="goToAbout">Go to About</button>
+  </div>
+  <div class="div" style="margin-bottom: 8px">
+    <button @click="showToast">Show Toast</button>
+  </div>
+  <div class="div" style="margin-bottom: 8px">
+    <button @click="showToast(false)">Show Error Toast</button>
+  </div>
+</template>
